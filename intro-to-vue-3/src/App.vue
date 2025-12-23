e38cd4078de5c773b4aaad75c6224ad968790767
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-12-23 13:04:08 +0700

    conditional rendering

--- intro-to-vue-3/src/App.vue
+++ intro-to-vue-3/src/App.vue
@@ -5,6 +5,7 @@ import socksGreenImage from "./assets/images/socks_green.jpeg";
 const product = ref("Socks");
 const description = ref("A pair of warm, fuzzy socks");
 const image = ref(socksGreenImage);
+const inventory = ref(100);
 </script>
 
 <template>
@@ -15,7 +16,15 @@ const image = ref(socksGreenImage);
       </div>
       <div class="w-1/2">
         <h1 class="text-5xl/tight mb-5">{{ product }}</h1>
-        <p class="text-xl">{{ description }}</p>
+        <p class="text-xl mb-3">{{ description }}</p>
+        <p class="text-lg font-semibold" v-if="inventory > 10">In Stock</p>
+        <p
+          class="text-lg font-semibold"
+          v-else-if="inventory <= 10 && inventory > 0"
+        >
+          Almost Sold Out!
+        </p>
+        <p class="text-lg font-semibold" v-else>Out of Stock</p>
       </div>
     </div>
   </div>
