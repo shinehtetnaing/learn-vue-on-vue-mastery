84f8df5121e01606b67ae14fde41f47f6671aa09
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-12-24 14:11:56 +0700

    event handling

--- intro-to-vue-3/src/App.vue
+++ intro-to-vue-3/src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 import { ref } from "vue";
 import socksGreenImage from "./assets/images/socks_green.jpeg";
+import socksBlueImage from "./assets/images/socks_blue.jpeg";
 
 const product = ref("Socks");
 const description = ref("A pair of warm, fuzzy socks");
 const image = ref(socksGreenImage);
+
 const inventory = ref(100);
+
 const varients = ref([
-  { id: 1, color: "green" },
-  { id: 2, color: "blue" },
+  { id: 1, color: "green", image: socksGreenImage },
+  { id: 2, color: "blue", image: socksBlueImage },
 ]);
+
 const listColor = (color) => {
   switch (color) {
     case "blue":
@@ -20,15 +24,36 @@ const listColor = (color) => {
       return "before:text-gray-400";
   }
 };
+
+const cart = ref(0);
+
+const addToCart = () => (cart.value += 1);
+
+const removeFromCart = () => {
+  if (cart.value >= 1) {
+    cart.value -= 1;
+  }
+};
+
+const updateImage = (varientImage) => {
+  image.value = varientImage;
+};
 </script>
 
 <template>
-  <div class="flex flex-col p-4">
-    <div class="flex gap-10">
-      <div class="w-1/2">
+  <nav class="w-full h-20 bg-green-300 px-8 flex justify-end items-center">
+    <div
+      class="rounded-lg border-2 border-gray-950 flex items-center justify-center text-xl font-semibold text-black p-5 h-10"
+    >
+      Cart ({{ cart }})
+    </div>
+  </nav>
+  <div class="flex flex-col px-8 py-12">
+    <div class="flex flex-col sm:flex-row gap-10">
+      <div class="w-sm sm:w-125">
         <img :src="image" class="shadow rounded-xl" />
       </div>
-      <div class="w-1/2">
+      <div>
         <h1 class="text-5xl/tight mb-5">{{ product }}</h1>
         <p class="text-xl mb-3">{{ description }}</p>
         <p class="text-lg font-semibold" v-if="inventory > 10">In Stock</p>
@@ -41,17 +66,22 @@ const listColor = (color) => {
         <p class="text-lg font-semibold" v-else>Out of Stock</p>
         <div class="mt-6">
           <h2 class="text-xl font-semibold mb-2">Varients</h2>
-          <ul class="list-none">
+          <ul class="list-none w-fit">
             <li
               v-for="varient in varients"
               :key="varient.id"
               class="before:content-['⬤'] before:mr-2 before:text-3xl flex items-center capitalize"
               :class="listColor(varient.color)"
+              @mouseover="updateImage(varient.image)"
             >
               {{ varient.color }}
             </li>
           </ul>
         </div>
+        <button class="mt-6 mr-5" @click="addToCart">Add to Cart</button>
+        <button class="mt-6 bg-gray-500!" @click="removeFromCart">
+          Remove from Cart
+        </button>
       </div>
     </div>
   </div>
